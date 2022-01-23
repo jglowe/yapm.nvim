e0101c026ad0447871fe5c03631a9a4284d4205e
Jonathan Lowe <me@jonathanlowe.dev>
2022-01-23 10:20:01 -0500

    Made list popup resizable, closable, and have a border

--- lua/yapm/list.lua
+++ lua/yapm/list.lua
@@ -15,15 +15,9 @@
 --------------------------------------------------------------------------------
 local state = require("yapm.state")
 
-local close_popup = function()
-    pcall(vim.api.nvim_win_close, state.get_popup_id(), true)
-    state.set_popup_id(nil)
-end
-
-local open_popup = function()
-
+local popup_contents = function()
     local plugin_list = state.get_loaded_plugins()
-    local popup_width = math.floor(vim.fn.winwidth(0) * .8)
+    local popup_width = math.floor(vim.opt.columns:get() * .8)
     local max_plugin_length = 0
 
     for _, plugin in pairs(plugin_list) do
@@ -59,27 +53,51 @@ local open_popup = function()
     table.insert(lines_to_show, current_line)
     table.insert(lines_to_show, "")
 
-    local bufnr = vim.api.nvim_create_buf(false, true)
-    assert(bufnr, "Failed to create buffer")
-    vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, lines_to_show)
+    return lines_to_show
+end
 
+local get_popup_options = function(height)
     local window_options = {}
     window_options.relative = "editor"
     window_options.style = "minimal"
-    window_options.width = popup_width
-    window_options.height = #lines_to_show
-    window_options.col = math.floor(
-                             (vim.fn.winwidth(0) - window_options.width) / 2)
+    window_options.width = math.floor(vim.opt.columns:get() * .8)
+    window_options.height = height
+    window_options.col = math.floor((vim.opt.columns:get() -
+                                        window_options.width) / 2)
     window_options.row = math.floor(
-                             (vim.fn.winheight(0) - window_options.height) / 2)
-    window_options.border = "single"
+                             (vim.opt.lines:get() - window_options.height) / 2)
+    window_options.border = "single" -- { '─', '│', '─', '│', '┌', '┐', '┘', '└'}
 
-    local popup_id = vim.api.nvim_open_win(bufnr, false, window_options)
+    return window_options
+end
+
+local close_popup = function()
+    pcall(vim.api.nvim_win_close, state.get_popup_id(), true)
+    state.set_popup_id(nil)
+end
+
+local resize_popup = function(bufnr)
+    local buf_contents = popup_contents()
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, buf_contents)
+
+    vim.api.nvim_win_set_config(state.get_popup_id(),
+                                get_popup_options(#buf_contents))
+end
+
+local open_popup = function()
+    local buf_contents = popup_contents()
+
+    local bufnr = vim.api.nvim_create_buf(false, true)
+    assert(bufnr, "Failed to create buffer")
+    vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, buf_contents)
+
+    local popup_id = vim.api.nvim_open_win(bufnr, false,
+                                           get_popup_options(#buf_contents))
     state.set_popup_id(popup_id)
-    -- state.set_popup_opts(popup_opts)
 
     print(popup_id)
+
     vim.api.nvim_set_current_win(popup_id)
 
     vim.api.nvim_buf_set_keymap(bufnr, "n", "<esc>",
@@ -87,8 +105,15 @@ local open_popup = function()
                                 {noremap = true})
 
     vim.cmd(
-        "autocmd BufLeave <buffer> ++once lua pcall(vim.api.nvim_win_close, require('yapm.state').get_popup_id(), true)")
+        "autocmd BufLeave <buffer> ++once lua require('yapm.list').close_popup()")
+    vim.cmd(
+        "autocmd VimResized <buffer> ++nested lua require('yapm.list').resize_popup(" ..
+            tostring(bufnr) .. ")")
 end
 
-return {open_popup = open_popup, close_popup = close_popup}
+return {
+    open_popup = open_popup,
+    close_popup = close_popup,
+    resize_popup = resize_popup
+}
 
